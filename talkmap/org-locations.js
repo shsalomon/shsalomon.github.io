f88b8ec3517a9f9596f2ba65eaa1cea69ee81ec3
shsalomon <130984885+shsalomon@users.noreply.github.com>
2024-07-31 16:03:16 +0200

    Add files via upload

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,1 +1,12 @@
-var addressPoints = [];
+var addressPoints = [
+  [
+    "Heidelberg University, Heidelberg, Germany",
+    49.41937455,
+    8.670220825559886
+  ],
+  [
+    "Pacifico Yokohama Conference Center, Yokohama, Japan",
+    35.4619497,
+    139.6343174
+  ]
+];
