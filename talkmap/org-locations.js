3243a1360714771017122ac90e608bacaeaebb50
shsalomon <130984885+shsalomon@users.noreply.github.com>
2025-10-01 01:36:55 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -8,5 +8,20 @@ var addressPoints = [
     "Pacifico Yokohama Conference Center, Yokohama, Japan",
     35.4619497,
     139.6343174
+  ],
+  [
+    "University of Leeds, Leeds, UK", 
+    53.80757, 
+    -1.554547
+  ],
+  [
+    "Grand Hotel Vesuvio, Sorrento, Italy", 
+   40.625833, 
+   14.358056
+  ],
+  [
+    "Vytautas Magnus University, Kaunas, Lithuania", 
+   54.898333, 
+   23.913889
   ]
 ];
